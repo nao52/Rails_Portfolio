0e6862ce85f591ae97cee0aa43934a1a67c587fc
nao52 <naoki.52abc@icloud.com>
2023-01-12 15:41:08 +0900

    ユーザーモデルの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_12_063835) do
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "profile"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
 end
