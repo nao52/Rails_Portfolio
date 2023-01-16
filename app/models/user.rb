4449c6531c354ac88224d31db0b9de54f73ef609
nao52 <naoki.52abc@icloud.com>
2023-01-16 17:19:28 +0900

    テスト用データの作成(db.seed)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :active_relationships,   lass_name:   "Relationship",
+  has_many :active_relationships,   class_name:   "Relationship",
                                     foreign_key:  "follower_id",
                                     dependent:    :destroy
   has_many :passive_relationships,  class_name:   "Relationship",
