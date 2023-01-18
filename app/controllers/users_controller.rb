28659f9077c327b346407c07de1645eeeaac09c9
nao52 <naoki.52abc@icloud.com>
2023-01-19 08:28:47 +0900

    ユーザー情報の編集ボタンを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def following
     @user  = User.find(params[:id])
     @title = "#{@user.name}のフォロー"
