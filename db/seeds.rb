58f6da39cec72900654f610371d749e69d2eab20
nao52 <naoki.52abc@icloud.com>
2023-01-18 13:39:06 +0900

    担当教科の表示を実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,3 @@
-# テストユーザーの作成
-100.times do |n|
-  name = "テストユーザー#{n+1}"
-  email = "test#{n+1}@net.com"
-  password = "password"
-  User.create!( name:                   name,
-                email:                  email,
-                password:               password,
-                password_confirmation:  password,
-              )
-end
-
-# ユーザーフォローのリレーションシップを作成
-users = User.all
-user = users.first
-following = users[2..80]
-followers = users[21..100]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
-
 # subjects_databaseに値をセット
 Subject.create!( name: "国語" )
 Subject.create!( name: "数学" )
@@ -28,3 +8,26 @@ Subject.create!( name: "体育" )
 Subject.create!( name: "家庭科" )
 Subject.create!( name: "音楽" )
 Subject.create!( name: "美術" )
+
+# テストユーザーの作成
+subject_size = Subject.all.size
+100.times do |n|
+  name = "テストユーザー#{n+1}"
+  email = "test#{n+1}@net.com"
+  password = "password"
+  subject_id = rand(subject_size) + 1 
+  User.create!( name:                   name,
+                email:                  email,
+                password:               password,
+                password_confirmation:  password,
+                subject_id:             subject_id
+              )
+end
+
+# ユーザーフォローのリレーションシップを作成
+users = User.all
+user = users.first
+following = users[2..80]
+followers = users[21..100]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
