84522906ae197de917fb69b44c29002173b39614
nao52 <naoki.52abc@icloud.com>
2023-01-19 17:04:56 +0900

    教科別の投稿一覧を実装

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -5,7 +5,8 @@ class SubjectsController < ApplicationController
   end
 
   def show
-    
+    @subject = Subject.find(params[:id])
+    @posts = SubjectPost.where(subject_id: params[:id])
   end
 
 end
