f57c723a3b50be1851d4f9866c9c6e5bf71711e7
nao52 <naoki.52abc@icloud.com>
2023-01-19 20:51:53 +0900

    校種別の投稿一覧ページを実装

--- app/controllers/kinds_of_schools_controller.rb
+++ app/controllers/kinds_of_schools_controller.rb
@@ -6,7 +6,7 @@ class KindsOfSchoolsController < ApplicationController
 
   def show
     @kinds_of_school = KindsOfSchool.find(params[:id])
-    @posts = KindsOfSchoolPosts.where(kinds_of_school_id: params[:id])
+    @posts = KindsOfSchoolPost.where(kinds_of_school_id: params[:id])
   end
 
 end
