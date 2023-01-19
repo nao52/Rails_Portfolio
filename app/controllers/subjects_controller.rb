f3035c38d8d9f244c02f60e45c927553d96e0577
nao52 <naoki.52abc@icloud.com>
2023-01-19 16:07:57 +0900

    教科一覧ページを実装

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,11 @@
+class SubjectsController < ApplicationController
+
+  def index
+    @subjects = Subject.all
+  end
+
+  def show
+    
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
     end
   end
   resources :relationships,   only: [:create, :destroy]
+  resources :subjects,        only: [:index, :show]
 end
