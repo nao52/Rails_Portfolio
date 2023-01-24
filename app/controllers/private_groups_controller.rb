fdb39d1122f68c835c59c13dbd51e42cde79dcac
nao52 <naoki.52abc@icloud.com>
2023-01-24 11:36:17 +0900

    プライベートグループの新規作成機能を実装

--- app/controllers/private_groups_controller.rb
+++ app/controllers/private_groups_controller.rb
@@ -1,4 +1,5 @@
 class PrivateGroupsController < ApplicationController
+  before_action :logged_in_user,      only: [:new, :create]
   before_action :set_private_group,   only: [:show, :members]
 
   def index
@@ -11,11 +12,16 @@ class PrivateGroupsController < ApplicationController
   end
 
   def new
-    @private_group = current_user.private_groups.build if logged_in?
+    @private_group = current_user.private_groups.build
   end
 
   def create
-
+    @private_group = current_user.private_groups.build(private_group_params)
+    if @private_group.save
+      redirect_to private_groups_url
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def members
@@ -26,5 +32,9 @@ class PrivateGroupsController < ApplicationController
 
     def set_private_group
       @group = PrivateGroup.find(params[:id])
+    end
+
+    def  private_group_params
+      params.require(:private_group).permit(:name, :detail)
     end
 end
