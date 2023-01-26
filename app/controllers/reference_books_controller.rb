9378f3d28bfeb4263bf97dd0702913ee1cdb4ff2
nao52 <naoki.52abc@icloud.com>
2023-01-26 14:59:58 +0900

    参考書の追加機能を実装

--- app/controllers/reference_books_controller.rb
+++ app/controllers/reference_books_controller.rb
@@ -1,4 +1,5 @@
 class ReferenceBooksController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create, :edit, :update]
   
   def index
     @reference_books = ReferenceBook.all
@@ -8,6 +9,22 @@ class ReferenceBooksController < ApplicationController
     @reference_book = current_user.reference_books.build(publisher_id: params[:publisher_id])
   end
 
+  def create
+    @reference_book = current_user.reference_books.build(reference_book_params)
+    if @reference_book.save
+      @publisher = Publisher.find(@reference_book.publisher_id)
+      redirect_to @publisher
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
   def edit
   end
+
+  private
+
+    def reference_book_params
+      params.require(:reference_book).permit(:title, :content, :publisher_id)
+    end
 end
