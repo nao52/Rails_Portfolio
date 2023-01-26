bda628ff80b4bc7de83ffdfdb8663f684ea3e97f
nao52 <naoki.52abc@icloud.com>
2023-01-26 16:12:38 +0900

    参考書情報の編集機能を実装

--- app/controllers/reference_books_controller.rb
+++ app/controllers/reference_books_controller.rb
@@ -1,6 +1,9 @@
 class ReferenceBooksController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create, :edit, :update]
-  before_action :correct_user,   only: [:edit, :update, :destroy]
+  before_action :logged_in_user,   only: [:new, :create, :edit, :update]
+  before_action :correct_user,     only: [:edit, :update, :destroy]
+  before_action -> {
+    create_publisher(params[:name], params[:reference_book][:publisher_id])
+  }, only: [:create, :update]
   
   def index
     @reference_books = ReferenceBook.all
@@ -10,17 +13,10 @@ class ReferenceBooksController < ApplicationController
     @reference_book = current_user.reference_books.build(publisher_id: params[:publisher_id])
   end
 
-  def create
-    if params[:name].present? && params[:reference_book][:publisher_id].blank?
-      new_publisher = current_user.publishers.create!(name: params[:name])
-      params[:reference_book][:publisher_id] = new_publisher.id
-    end
-    
+  def create    
     @reference_book = current_user.reference_books.build(reference_book_params)
-
     if @reference_book.save
-      @publisher = Publisher.find(@reference_book.publisher_id)
-      redirect_to @publisher
+      redirect_to @reference_book.publisher
     else
       render 'new', status: :unprocessable_entity
     end
@@ -30,6 +26,11 @@ class ReferenceBooksController < ApplicationController
   end
 
   def update
+    if @reference_book.update(reference_book_params)
+      redirect_to @reference_book.publisher
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   private
@@ -44,6 +45,13 @@ class ReferenceBooksController < ApplicationController
     def correct_user
       @reference_book = current_user.reference_books.find_by(id: params[:id])
       redirect_to root_url, status: :see_other if @reference_book.nil?
+    end
+
+    def create_publisher(publisher_name, publisher_id)
+      if publisher_id.blank? && publisher_name.present?
+        new_publisher = current_user.publishers.create!(name: params[:name])
+        params[:reference_book][:publisher_id] = new_publisher.id
+      end
     end
 
 end
