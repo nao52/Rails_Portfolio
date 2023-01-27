cc6ff957c368e13cebbf59a07c21c0715e77823b
nao52 <naoki.52abc@icloud.com>
2023-01-27 17:20:52 +0900

    ワークシートの編集フォームを実装

--- app/controllers/worksheets_controller.rb
+++ app/controllers/worksheets_controller.rb
@@ -1,5 +1,6 @@
 class WorksheetsController < ApplicationController
   before_action :logged_in_user,   only: [:new, :create]
+  before_action :correct_user,     only: [:edit, :update]
 
   def new
     @worksheet = current_user.worksheets.build
@@ -15,10 +16,21 @@ class WorksheetsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   private
 
     def worksheet_params
       params.require(:worksheet).permit(:name, :detail, :file)
+    end
+
+    # before フィルタ
+    
+    # 現在のユーザーが作成者であるか確認
+    def correct_user
+      @worksheet = current_user.worksheets.find_by(id: params[:id])
+      redirect_to root_url, status: :see_other if @worksheet.nil?
     end
 
 end
