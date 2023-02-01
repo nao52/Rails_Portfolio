a1870c2f9098cd54d7c5d04519ac40c0dc3cd2a3
nao52 <naoki.52abc@icloud.com>
2023-02-01 16:17:01 +0900

    生徒の名前入力を実装

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,14 +1,25 @@
 class TestsController < ApplicationController
 
   def crate_seats
-    flash[:col] = params[:col]
-    flash[:row] = params[:row]
+    col = params[:col].to_i
+    row = params[:row].to_i
+
+    names = []
+    (col*row).times do |n|
+      names << params["name#{n+1}"]
+    end
+
+    flash[:col] = params[:col].to_i
+    flash[:row] = params[:row].to_i
+    flash[:names] = names
+    
     redirect_to test_url
   end
 
   def seat_create
     @col = 6
     @row = 6
+    @names = []
   end
 
 end
