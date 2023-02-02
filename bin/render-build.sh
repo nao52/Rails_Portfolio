a0a2cfa58c696565d436083286ee149f7b6d2182
nao52 <naoki.52abc@icloud.com>
2023-02-02 17:26:46 +0900

    本番環境でのデータベースの設定を変更(削除禁止)

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,8 +4,8 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# bundle exec rails db:migrate
+bundle exec rails db:migrate
 
 # テストデータを導入する際に使用
-bundle exec rails db:migrate:reset
-bundle exec rails db:seed
+# bundle exec rails db:migrate:reset
+# bundle exec rails db:seed
