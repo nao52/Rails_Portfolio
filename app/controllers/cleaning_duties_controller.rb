02003122dc3da231dca5559247de85d3cbeedd06
nao52 <naoki.52abc@icloud.com>
2023-02-03 12:13:52 +0900

    グループ数に応じたテーブルの作成機能を実装

--- app/controllers/cleaning_duties_controller.rb
+++ app/controllers/cleaning_duties_controller.rb
@@ -2,10 +2,12 @@ class CleaningDutiesController < ApplicationController
 
   def show
     @student_num = 30
+    @group_num   = 5
   end
 
   def create_list
-    flash[:student_num] = params[:student_num]
+    flash[:student_num] = params[:student_num] if params[:student_num]
+    flash[:group_num]   = params[:group_num] if params[:group_num]
     redirect_to cleaning_duty_show_url
   end
 
