f3d48fecb329f284aa1aed550660d3754826ddda
nao52 <naoki.52abc@icloud.com>
2023-02-04 22:00:40 +0900

    コードのリファクタリング

--- app/controllers/cleaning_duties_controller.rb
+++ app/controllers/cleaning_duties_controller.rb
@@ -2,7 +2,10 @@ class CleaningDutiesController < ApplicationController
   before_action :set_hash
 
   def show
-    @group_size    = 4
+    4.times do |n|
+      @groups["#{n+1}"] = { name: "", boys_size: 4, girls_size: 4, total_size: 8}
+    end
+    @group_size    = @groups.size
     @student_size  = 32
   end
 
@@ -11,16 +14,21 @@ class CleaningDutiesController < ApplicationController
     group_size     = params[:group_size].to_i
     student_size   = 0
 
-    group_size.times do |n|
+    new_group_size.times do |n|
       boys_size  = params["boys_size#{n+1}"].to_i
+      boys_size  = 4 if boys_size == 0
       girls_size = params["girls_size#{n+1}"].to_i
+      girls_size = 4 if girls_size == 0
+      total_size = boys_size + girls_size
       student_size += boys_size + girls_size
+      @group_names["#{n+1}"] = params["group_name#{n+1}"]
       @boys_sizes["#{n+1}"] = boys_size
       @girls_sizes["#{n+1}"] = girls_size
-      @group_names["#{n+1}"] = params["group_name#{n+1}"]
+      name = params["group_name#{n+1}"] || ""
+      @groups["#{n+1}"] = { name: name, boys_size: boys_size, girls_size: girls_size, total_size: total_size }
     end
 
-    student_size += (new_group_size - group_size) * 8
+    # student_size += (new_group_size - group_size) * 8
     
     student_size.times do |n|
       if params["student_name#{n+1}"].nil?
@@ -46,6 +54,7 @@ class CleaningDutiesController < ApplicationController
       @girls_sizes   = {}
       @group_names   = {}
       @student_names = {}
+      @groups        = {}
     end
     
 
