9e8cd509aa4a68d96eb679ff0044303271bf630f
nao52 <naoki.52abc@icloud.com>
2023-02-06 18:53:09 +0900

    科目の追加 / 日課表へのデータのセットを実装

--- app/controllers/calc_absents_controller.rb
+++ app/controllers/calc_absents_controller.rb
@@ -1,13 +1,15 @@
 class CalcAbsentsController < ApplicationController
+  before_action :set_carriculums
 
   def show
-    @carriculum_size = 15
-    @carriculums = []
   end
 
   def set_curriculum
-    @carriculum_size = 15
-    @carriculums = []
+    30.times do |n|
+      @schedules << params["schedule#{n+1}"]
+    end
+    puts "テスト=#{@schedules}"
+
     @carriculum_size.times do |n|
       @carriculums << params["carriculums#{n+1}"] if params["carriculums#{n+1}"].present?
     end
@@ -15,13 +17,27 @@ class CalcAbsentsController < ApplicationController
   end
 
   def set_test_data
-    @carriculum_size = 15
-    @carriculums = []
     @carriculums = ["英語1", "芸術", "国語1", "理科基礎1", "数学1", "数学A", "体育", "総合的な学習の時間", "LHR", "現代社会", "地理A", "家庭科基礎"]
     render 'show', status: :unprocessable_entity
   end
 
   def schedule
+    @carriculums = params[:carriculums].split if params[:carriculums].present?
+    30.times do |n|
+      @schedules << params["suchedule#{n+1}"]
+    end
+    render 'show', status: :unprocessable_entity
   end
+
+  private
+
+    # beforeフィルタ
+
+    # カリキュラム情報をセット
+    def set_carriculums
+      @carriculum_size = 15
+      @carriculums = []
+      @schedules   = []
+    end
 
 end
