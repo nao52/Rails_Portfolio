cfcf2efced43330c4f4a10ed666007c76e95ea15
nao52 <naoki.52abc@icloud.com>
2023-02-07 15:42:22 +0900

    本番環境にデータベースが反映されないように設定を変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,16 +2,16 @@
 # exit on error
 
 # 通常はこちらを使用
-# set -o errexit
-# bundle install
-# bundle exec rails assets:precompile
-# bundle exec rails assets:clean
-# bundle exec rails db:migrate
-
-# テストデータを導入する際に使用
 set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate:reset
-bundle exec rails db:seed
+bundle exec rails db:migrate
+
+# テストデータを導入する際に使用
+# set -o errexit
+# bundle install
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+# bundle exec rails db:migrate:reset
+# bundle exec rails db:seed
