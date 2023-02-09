ed0f35a8c1b3cc21806c474cf47c05f014bea42b
nao52 <naoki.52abc@icloud.com>
2023-02-09 12:47:29 +0900

    ユーザーモデルのバリデーションテスト(empty / length)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,14 @@
 class User < ApplicationRecord
   belongs_to :subject
-  validates :subject_id, presence: true
   belongs_to :club
-  validates :club_id, presence: true
   belongs_to :kinds_of_school
-  validates :kinds_of_school_id, presence: true
-
+  
   has_many :active_relationships,   class_name:   "Relationship",
-                                    foreign_key:  "follower_id",
-                                    dependent:    :destroy
+  foreign_key:  "follower_id",
+  dependent:    :destroy
   has_many :passive_relationships,  class_name:   "Relationship",
-                                    foreign_key:  "followed_id",
-                                    dependent:    :destroy
+  foreign_key:  "followed_id",
+  dependent:    :destroy
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :subject_posts, dependent: :destroy
@@ -32,21 +29,25 @@ class User < ApplicationRecord
   has_many :worksheet_favorites, dependent: :destroy
   has_many :favorite_worksheets, through: :worksheet_favorites, source: :worksheet
   has_many :book_reviews
-
+  
   before_save { email.downcase! }
+  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  format: { with: VALID_EMAIL_REGEX },
+  uniqueness: { case_sensitive: false }
   validates :profile, length: { maximum: 140 }
   has_secure_password
   validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
   validates :image,    content_type:  { in: %w[image/jpeg image/gif image/png],
-                                        message: "形式は「jpeg / gif / png」のいずれかにしてください" },
-                       size:          { less_than: 5.megabytes,
-                                        message: "画像は5MB以下にしてください"}
-
+    message: "形式は「jpeg / gif / png」のいずれかにしてください" },
+    size:          { less_than: 5.megabytes,
+      message: "画像は5MB以下にしてください"}
+  validates :subject_id, presence: true
+  validates :club_id, presence: true
+  validates :kinds_of_school_id, presence: true
+      
   # ユーザーをフォローする
   def follow(other_user)
     following << other_user unless self == other_user
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,68 @@ RSpec.describe User, type: :model do
     expect(FactoryBot.build(:user)).to be_valid
   end
 
+  it "is invalid without name" do
+    expect(FactoryBot.build(:user, name: "")).to_not be_valid
+  end
+
+  it "is invalid without email" do
+    expect(FactoryBot.build(:user, email: "")).to_not be_valid
+  end
+
+  it "is invalid without password" do
+    expect(FactoryBot.build(:user, password: "")).to_not be_valid
+  end
+
+  it "is invalid without subject_id" do
+    expect(FactoryBot.build(:user, subject_id: "")).to_not be_valid
+  end
+
+  it "is invalid without club_id" do
+    expect(FactoryBot.build(:user, club_id: "")).to_not be_valid
+  end
+
+  it "is invalid without kinds_of_school_id" do
+    expect(FactoryBot.build(:user, kinds_of_school_id: "")).to_not be_valid
+  end
+
+  it "is valid when name is less than 50 characters" do
+    valid_name = "a" * 50
+    expect(FactoryBot.build(:user, name: valid_name)).to be_valid
+  end
+
+  it "is invalid when name is more 51 characters" do
+    invalid_name = "a" * 51
+    expect(FactoryBot.build(:user, name: invalid_name)).to_not be_valid
+  end
+
+  it "is valid when email is less than 255 characters" do
+    valid_email = ("a" * 243) + "@example.com"
+    expect(FactoryBot.build(:user, email: valid_email)).to be_valid
+  end
+
+  it "is invalid when email is more 256 characters" do
+    invalid_email = ("a" * 244) + "@example.com"
+    expect(FactoryBot.build(:user, email: invalid_email)).to_not be_valid
+  end
+
+  it "is valid when profile is less than 140 characters" do
+    valid_profile = "a" * 140
+    expect(FactoryBot.build(:user, profile: valid_profile)).to be_valid
+  end
+
+  it "is invalid when profile is more 141 characters" do
+    invalid_profile = "a" * 141
+    expect(FactoryBot.build(:user, profile: invalid_profile)).to_not be_valid
+  end
+
+  it "is valid when password is more 8 characters" do
+    valid_password = "a" * 8
+    expect(FactoryBot.build(:user, password: valid_password)).to be_valid
+  end
+
+  it "is invalid when password is less than 7 characters" do
+    invalid_password = "a" * 7
+    expect(FactoryBot.build(:user, password: invalid_password)).to_not be_valid
+  end
+
 end
