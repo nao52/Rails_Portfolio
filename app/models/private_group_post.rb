9c1f0df266366c5c1a6b1f2de6612646ab0b5dc2
nao52 <naoki.52abc@icloud.com>
2023-02-10 12:02:42 +0900

    private_group_postモデルのテストを実装

--- app/models/private_group_post.rb
+++ app/models/private_group_post.rb
@@ -3,8 +3,8 @@ class PrivateGroupPost < ApplicationRecord
   belongs_to :private_group
 
   default_scope -> { order(created_at: :desc) }
-
+  
+  validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
   validates :private_group_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
 end
