1754aff35d836025fbecbdb7f393ee97ef30036f
nao52 <naoki.52abc@icloud.com>
2023-02-10 16:14:36 +0900

    kinds_of_schoolモデルのテストをリファクタリング

--- spec/models/kinds_of_school_spec.rb
+++ spec/models/kinds_of_school_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe KindsOfSchool, type: :model do
 
+  let(:kinds_of_school) { FactoryBot.build(:kinds_of_school) }
+
   it "is valid with name" do
-    expect(FactoryBot.build(:kinds_of_school)).to be_valid
+    expect(kinds_of_school).to be_valid
   end
 
   it "is invalid without name" do
-    expect(FactoryBot.build(:kinds_of_school, name: "")).to_not be_valid
+    kinds_of_school.name = ""
+    expect(kinds_of_school).to_not be_valid
+  end
+
+  it "is valid when name is less than 50 characters" do
+    kinds_of_school.name = "a" * 50
+    expect(kinds_of_school).to be_valid
+  end
+
+  it "is invalid when name is more 51 characters" do
+    kinds_of_school.name = "a" * 51
+    expect(kinds_of_school).to_not be_valid
   end
 
 end
