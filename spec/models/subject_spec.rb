fa712f6db39917d7f9a279f962033003a52a14c5
nao52 <naoki.52abc@icloud.com>
2023-02-10 16:04:46 +0900

    subjectモデルのテストをリファクタリング

--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Subject, type: :model do
 
+  let(:subject) { FactoryBot.build(:subject) }
+
   it "is valid with name" do
-    expect(FactoryBot.build(:subject)).to be_valid
+    expect(subject).to be_valid
   end
 
   it "is invalid without name" do
-    expect(FactoryBot.build(:subject, name: "")).to_not be_valid
+    subject.name = ""
+    expect(subject).to_not be_valid
+  end
+
+  it "is valid when name is less than 50 characters" do
+    subject.name = "a" * 50
+    expect(subject).to be_valid
+  end
+
+  it "is invalid when name is more 51 characters" do
+    subject.name = "a" * 51
+    expect(subject).to_not be_valid
   end
 
 end
