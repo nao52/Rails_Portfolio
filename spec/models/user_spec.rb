6bcb121d53fcdc61d45b478cdcb48e6d2b2eb2de
nao52 <naoki.52abc@icloud.com>
2023-02-17 15:14:30 +0900

    バリデーションメッセージの設定を追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,26 +25,31 @@ RSpec.describe User, type: :model do
   it "is invalid without name" do
     @user1.name = ""
     expect(@user1).to_not be_valid
+    expect(@user1.errors.full_messages.include?("名前は必須項目です")).to be_truthy
   end
 
   it "is invalid without email" do
     @user1.email = ""
     expect(@user1).to_not be_valid
+    expect(@user1.errors.full_messages.include?("メールアドレスは必須項目です")).to be_truthy
   end
 
   it "is invalid without subject_id" do
     @user1.subject_id = nil
     expect(@user1).to_not be_valid
+    expect(@user1.errors.full_messages.include?("教科を選択してください")).to be_truthy
   end
 
   it "is invalid without club_id" do
     @user1.club_id = nil
     expect(@user1).to_not be_valid
+    expect(@user1.errors.full_messages.include?("部活動を選択してください")).to be_truthy
   end
 
   it "is invalid without kinds_of_school_id" do
     @user1.kinds_of_school_id = nil
     expect(@user1).to_not be_valid
+    expect(@user1.errors.full_messages.include?("校種を選択してください")).to be_truthy
   end
 
   it "is valid when name is less than 50 characters" do
@@ -55,6 +60,7 @@ RSpec.describe User, type: :model do
   it "is invalid when name is more 51 characters" do
     @user1.name = "a" * 51
     expect(@user1).to_not be_valid
+    expect(@user1.errors.full_messages.include?("名前は50文字以内で入力してください")).to be_truthy
   end
 
   it "is valid when email is less than 255 characters" do
@@ -65,6 +71,7 @@ RSpec.describe User, type: :model do
   it "is invalid when email is more 256 characters" do
     @user1.email = ("a" * 244) + "@example.com"
     expect(@user1).to_not be_valid
+    expect(@user1.errors.full_messages.include?("メールアドレスは255文字以内で入力してください")).to be_truthy
   end
 
   it "is valid when profile is less than 140 characters" do
@@ -75,6 +82,7 @@ RSpec.describe User, type: :model do
   it "is invalid when profile is more 141 characters" do
     @user1.profile = "a" * 141
     expect(@user1).to_not be_valid
+    expect(@user1.errors.full_messages.include?("プロフィールは140文字以内で入力してください")).to be_truthy
   end
 
   it "is valid when password is more 8 characters" do
@@ -85,6 +93,18 @@ RSpec.describe User, type: :model do
   it "is invalid when password is less than 7 characters" do
     @user1.password = @user1.password_confirmation = "a" * 7
     expect(@user1).to_not be_valid
+    expect(@user1.errors.full_messages.include?("パスワードは8文字以上で入力してください")).to be_truthy
+  end
+
+  it "is valid when password is less than 16 characters" do
+    @user1.password = @user1.password_confirmation = "a" * 16
+    expect(@user1).to be_valid
+  end
+
+  it "is invalid when password is more 17 characters" do
+    @user1.password = @user1.password_confirmation = "a" * 17
+    expect(@user1).to_not be_valid
+    expect(@user1.errors.full_messages.include?("パスワードは16文字以内で入力してください")).to be_truthy
   end
 
   it "is valid with valid email" do
@@ -102,13 +122,15 @@ RSpec.describe User, type: :model do
     invalid_emails.each do |invalid_email|
       @user1.email = invalid_email
       expect(@user1).to_not be_valid
+      expect(@user1.errors.full_messages.include?("メールアドレスの形式が不正です")).to be_truthy
     end
   end
 
-  it "is valid when email is unique" do
+  it "is invalid when email is unique" do
     duplicate_user = @user1.dup
     duplicate_user.email = @user1.email.upcase
     expect(duplicate_user).to_not be_valid
+    expect(duplicate_user.errors.full_messages.include?("メールアドレスはすでに存在します")).to be_truthy
   end
 
   it "follow and unfollow a user" do
