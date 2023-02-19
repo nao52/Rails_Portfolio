244a467c32ab58a390633fd4cf1d6804b6b6fdeb
nao52 <naoki.52abc@icloud.com>
2023-02-19 18:28:59 +0900

    club/club_postモデルのテストにエラーメッセージを追加

--- app/models/club_post.rb
+++ app/models/club_post.rb
@@ -5,6 +5,4 @@ class ClubPost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
 
   validates :content, presence: true, length: { maximum: 140 }
-  validates :user_id, presence: true
-  validates :club_id, presence: true
 end
--- spec/models/club_post_spec.rb
+++ spec/models/club_post_spec.rb
@@ -3,15 +3,11 @@ require 'rails_helper'
 RSpec.describe ClubPost, type: :model do
 
   before do
-    @subject         = FactoryBot.create(:subject)
-    @club            = FactoryBot.create(:club)
-    @kinds_of_school = FactoryBot.create(:kinds_of_school)
-    @user            = FactoryBot.create(:user, subject_id: @subject.id,
-                                                club_id:  @club.id,
-                                                kinds_of_school_id: @kinds_of_school.id)
+    @user = FactoryBot.create(:user)
   end
 
-  let(:post) { FactoryBot.create(:club_post, user_id: @user.id, club_id: @club.id) }
+  let(:post) { FactoryBot.create(:club_post, user_id: @user.id, club_id: Club.first.id) }
+  let(:club) { Club.first }
 
   it "is valid with content, user_id, club_id" do
     expect(post).to be_valid
@@ -20,6 +16,7 @@ RSpec.describe ClubPost, type: :model do
   it "is invalid without content" do
     post.content = ""
     expect(post).to_not be_valid
+    expect(post.errors.full_messages).to include("投稿内容は必須項目です")
   end
 
   it "is invalid without user_id" do
@@ -40,13 +37,14 @@ RSpec.describe ClubPost, type: :model do
   it "is invalid when content is more 141 characters" do
     post.content = "a" * 141
     expect(post).to_not be_valid
+    expect(post.errors.full_messages).to include("投稿内容は140文字以内で入力してください")
   end
 
   it "is first for most recent" do
     3.times do
-      FactoryBot.create(:club_post, user_id: @user.id, club_id: @club.id, created_at: 3.years.ago)
+      FactoryBot.create(:club_post, user_id: @user.id, club_id: club.id, created_at: 3.years.ago)
     end
-    most_recent_post = FactoryBot.create(:club_post, user_id: @user.id, club_id: @club.id, created_at: Time.zone.now)
+    most_recent_post = FactoryBot.create(:club_post, user_id: @user.id, club_id: club.id, created_at: Time.zone.now)
     expect(most_recent_post).to eq(ClubPost.first)
   end
 
