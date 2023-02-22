f583c6085f81e8b5621f3aacab6984deca9e73d1
nao52 <naoki.52abc@icloud.com>
2023-02-22 17:33:57 +0900

    グループ情報の編集機能のテストを実装

--- spec/features/private_group/private_group_update_spec.rb
+++ spec/features/private_group/private_group_update_spec.rb
@@ -33,6 +33,34 @@ RSpec.feature "PrivateGroupUpdates", type: :feature do
       expect(page).to have_link @private_group.name, href: private_group_path(@private_group)
       expect(page).to_not have_link "グループ情報の編集", href: edit_private_group_path(@private_group)
     end
+
+    scenario "update private_group is success" do
+      login(@michael)
+      visit root_path
+      click_link "グループ一覧"
+      click_link "グループ情報の編集"
+
+      expect {
+        fill_in "private_group[name]",    with: "グループ(更新)"
+        click_button "グループ情報の編集"
+      }.to change { current_path }.to(private_groups_path)
+
+      expect(page).to have_text("『グループ情報を更新しました！』")
+      expect(page).to have_link "グループ(更新)", href: private_group_path(@private_group)
+    end
+
+    scenario "update private_group is failed" do
+      login(@michael)
+      visit root_path
+      click_link "グループ一覧"
+      click_link "グループ情報の編集"
+      
+      fill_in "private_group[name]",    with: ""
+      click_button "グループ情報の編集"
+
+      expect(page).to have_text("『グループ情報の編集に失敗しました...』")
+      expect(page).to have_css('div#validation_messages')
+    end
   end
 
 end
