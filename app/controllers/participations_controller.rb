c0c5f26b70fed70f1207700833d595cac8872209
nao52 <naoki.52abc@icloud.com>
2023-03-04 13:10:46 +0900

    グループ参加ボタンに非同期処理を実装

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,15 +1,21 @@
 class ParticipationsController < ApplicationController
 
   def create
-    group = PrivateGroup.find(params[:private_group_id])
-    current_user.join(group)
-    redirect_to group
+    @group = PrivateGroup.find(params[:private_group_id])
+    current_user.join(@group)
+    respond_to do |format|
+      format.html { redirect_to @group }
+      format.turbo_stream
+    end
   end
 
   def destroy
-    group = Participation.find(params[:id]).private_group
-    current_user.leave(group)
-    redirect_back(fallback_location: root_url, status: :see_other)
+    @group = Participation.find(params[:id]).private_group
+    current_user.leave(@group)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_url, status: :see_other) }
+      format.turbo_stream
+    end
   end
 
 end
